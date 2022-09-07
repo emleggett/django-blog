a6be1daab1fd2dfbdc83c90cf41e821c97a23ca6
emleggett <erin.m.leggett@gmail.com>
2022-09-06 17:52:07 -0700

    Committing code from lesson 07/activity

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polling',
-    'blogging'
+    'blogging',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login/'
+
+LOGIN_REDIRECT_URL = '/'
