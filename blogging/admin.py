b52eb1c5f76077d23c5f4ebd93af2642462a9cac
emleggett <erin.m.leggett@gmail.com>
2022-09-07 15:00:07 -0700

    Adding code that hasn't been committed yet

---
+++ blogging/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from blogging.models import Post, Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+
+class CategoryInline(admin.StackedInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
