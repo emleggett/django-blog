3dc76e5312a09d546611730918a10288afd376ee
emleggett <erin.m.leggett@gmail.com>
2022-09-22 09:16:45 -0700

    Assignment 08 corrections

--- blogging/urls.py
+++ blogging/urls.py
@@ -4,12 +4,5 @@ from blogging.views import PostListView, PostDetailView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="blog_index"),
-    path("polls/<int:pk>/", PostDetailView.as_view(), name="blog_detail"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="blog_detail"),
 ]
-
-# from blogging.views import list_view, detail_view
-#
-# urlpatterns = [
-#     path('', list_view, name="blog_index"),
-#     path('posts/<int:post_id>/', detail_view, name="blog_detail")
-# ]
--- polling/urls.py
+++ polling/urls.py
@@ -6,10 +6,3 @@ urlpatterns = [
     path("", PollListView.as_view(), name="poll_index"),
     path("polls/<int:pk>/", PollDetailView.as_view(), name="poll_detail"),
 ]
-
-# from polling.views import list_view, detail_view
-
-# urlpatterns = [
-#     path('', list_view, name="poll_index"),
-#     path('polls/<int:poll_id>', detail_view, name="poll_detail")
-# ]
