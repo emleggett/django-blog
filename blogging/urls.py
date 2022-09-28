cfcc7ee19d4722dfd33dc967f4a175187cb14c72
emleggett <erin.m.leggett@gmail.com>
2022-09-28 16:39:21 -0700

    Pushing RSS feed to development branch

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from blogging.views import PostListView, PostDetailView
+from mysite.blogging.views import PostListView, PostDetailView, LatestEntriesFeed
 
 
 urlpatterns = [
     path("", PostListView.as_view(), name="blog_index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="blog_detail"),
+    path("posts/feed/", LatestEntriesFeed()),
 ]
