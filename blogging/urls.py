5764e5b5257089cb7cacf6b54506589e4cff1564
emleggett <erin.m.leggett@gmail.com>
2022-09-28 17:19:01 -0700

    Debugging

--- blogging/urls.py
+++ blogging/urls.py
@@ -5,5 +5,5 @@ from blogging.views import PostListView, PostDetailView, LatestEntriesFeed
 urlpatterns = [
     path("", PostListView.as_view(), name="blog_index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="blog_detail"),
-    path("feed/", LatestEntriesFeed()),
+    path(r"feed/", LatestEntriesFeed()),
 ]
